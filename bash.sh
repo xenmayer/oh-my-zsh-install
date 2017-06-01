483ace9b22189b791ffc5a7478945e9c1208e30c
Nikita <xenmayer@gmail.com>
2017-06-01 19:17:53 +0300

    Correct path

--- bash.sh
+++ bash.sh
@@ -4,8 +4,8 @@
 # instal oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-cp -f ~/app_config/oh-my-zsh/.zshrc ~/.zshrc
-cp -f ~/app_config/oh-my-zsh/themes/amuse.zsh-theme ~/.oh-my-zsh/themes/amuse.zsh-theme
+cp -f ~/oh-my-zsh-install/oh-my-zsh/.zshrc ~/.zshrc
+cp -f ~/oh-my-zsh-install/oh-my-zsh/themes/amuse.zsh-theme ~/.oh-my-zsh/themes/amuse.zsh-theme
 
 
 # install powerline fonts
